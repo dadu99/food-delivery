fa5bfc9cb62632c62803cc35a91a0795a17f750b
Darius Baciu <baciu.darius01@gmail.com>
2024-09-04 16:13:50 +0300

    food display and food item

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from "./context/StoreContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
