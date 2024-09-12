fdfc36c83d8adbbc096b213dc282044fc6630908
Darius Baciu <baciu.darius01@gmail.com>
2024-09-12 10:22:42 +0300

    add food item endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js";
+
+//dotenv
+import dotenv from 'dotenv'
+import foodRouter from "./routes/foodRoute.js";
+dotenv.config()
 
 
 //app config
@@ -11,9 +17,17 @@ const port = 4000;
 app.use(express.json())
 app.use(cors())
 
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))   //to display in browser each image
+
 app.get("/" , (req, res) => {
     res.send("API Working")
 })
+
 
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
